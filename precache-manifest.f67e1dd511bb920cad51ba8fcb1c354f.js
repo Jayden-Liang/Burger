865534070b235be2c2651d31892d7a22d643c5b1
Jayden-Liang <liangjisong@foxmail.com>
2019-10-31 22:54:37 +0800

    Updates

---
+++ precache-manifest.f67e1dd511bb920cad51ba8fcb1c354f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe42c0061fee490d07a43eb52dd407d1",
+    "url": "/Burger/index.html"
+  },
+  {
+    "revision": "eb3fb9f3e371224e857a",
+    "url": "/Burger/static/css/main.1c5e4f65.chunk.css"
+  },
+  {
+    "revision": "8f7796e998b606ef190c",
+    "url": "/Burger/static/js/2.1c6fe0ea.chunk.js"
+  },
+  {
+    "revision": "eb3fb9f3e371224e857a",
+    "url": "/Burger/static/js/main.81ea821b.chunk.js"
+  },
+  {
+    "revision": "ba358d9ecfd880b6ddc6",
+    "url": "/Burger/static/js/runtime~main.8dfb21fd.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
